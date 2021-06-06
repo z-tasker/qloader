37810892d42eb3d171bba31e1681516024d14821
mgrasker <mgraskertheband@gmail.com>
2021-06-06 15:06:29 -0400

    docker mount volume

--- bin/docker-google-images-search.sh
+++ bin/docker-google-images-search.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+OUTPUT_PATH="/tmp/qloader"
 VERSION=$(cat "./pyproject.toml" | sed -n 's/^version = "\(.*\)"$/\1 /gp' | xargs)
 
-docker run ialcloud/qloader:${VERSION} ${@}
+mkdir -p ${OUTPUT_PATH}
+
+docker run -v ${OUTPUT_PATH}:${OUTPUT_PATH} ialcloud/qloader:${VERSION} --output-path ${OUTPUT_PATH} ${@}
